f482832a90d246fb5c30bc8e84071878345aba1e
nahyun0423 <skgus0423@naver.com>
2025-02-07 17:19:26 +0900

    initial commit

---
+++ java-goods-management/src/main/java/com/j/goods_management/JavaGoodsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.j.goods_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGoodsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGoodsManagementApplication.class, args);
+	}
+
+}
---
+++ java-goods-management/src/test/java/com/j/goods_management/JavaGoodsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j.goods_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGoodsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
